e1c85b0ce37851a0a65175367bb717043bcfb540
jinio <jinioh88@gmail.com>
2020-03-03 08:48:06 +0900

    refactor: 가독성을 위한 리팩토링

--- test/ch2/ProfileTest.java
+++ test/ch2/ProfileTest.java
@@ -20,13 +20,8 @@ public class ProfileTest {
 
     @Test
     public void matchAnswersFalseWhenMustMathcCriteraNotMet() {
-        Answer profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
-
-        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-        Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-
-        criteria.add(criterion);
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
 
         boolean matches = profile.matches(criteria);
 
@@ -35,13 +30,9 @@ public class ProfileTest {
 
     @Test
     public void matchAnswersTrueForAnyDontCareCriteria() {
-        Answer profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
+        profile.add(new Answer(question, Bool.FALSE));
 
-        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
-
-        criteria.add(criterion);
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
 
         boolean matches = profile.matches(criteria);
 
