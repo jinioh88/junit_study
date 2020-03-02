8d6b62ae43d78fd3a23729e56ab997361ff4508c
jinio <jinioh88@gmail.com>
2020-03-03 08:40:44 +0900

    add: 두 번째 테스트

--- test/ch2/ProfileTest.java
+++ test/ch2/ProfileTest.java
@@ -3,6 +3,7 @@ package ch2;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ProfileTest {
     @Test
@@ -22,5 +23,24 @@ public class ProfileTest {
         boolean matches = profile.matches(criteria);
 
         assertFalse(matches);
+    }
+
+    @Test
+    public void matchAnswersTrueForAnyDontCareCriteria() {
+        Profile profile = new Profile("Bull Hockey, Inc.");
+        Question question = new BooleanQuestion(1, "Got bouneses?");
+
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+
+        assertTrue(matches);
     }
 }
