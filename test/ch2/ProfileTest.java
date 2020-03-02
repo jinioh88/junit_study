c6224eaac439d9bacdd25a49670ecf6ba7616918
jinio <jinioh88@gmail.com>
2020-03-03 08:45:57 +0900

    refactor: @Before 사용

--- test/ch2/ProfileTest.java
+++ test/ch2/ProfileTest.java
@@ -1,20 +1,28 @@
 package ch2;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ProfileTest {
+    private Profile profile;
+    private BooleanQuestion question;
+    private Criteria criteria;
+
+    @Before
+    public void create() {
+        profile = new Profile("Bull Hockey, Inc.");
+        question = new BooleanQuestion(1, "Got bouneses?");
+        criteria = new Criteria();
+    }
+
     @Test
     public void matchAnswersFalseWhenMustMathcCriteraNotMet() {
-        Profile profile = new Profile("Bull Hockey, Inc.");
-        Question question = new BooleanQuestion(1, "Got bouneses?");
-
         Answer profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
 
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
 
@@ -27,13 +35,9 @@ public class ProfileTest {
 
     @Test
     public void matchAnswersTrueForAnyDontCareCriteria() {
-        Profile profile = new Profile("Bull Hockey, Inc.");
-        Question question = new BooleanQuestion(1, "Got bouneses?");
-
         Answer profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
 
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
 
